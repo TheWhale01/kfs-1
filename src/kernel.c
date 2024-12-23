f1eacc92e7a1d531745163dcc0a9e8576bf1dfcb
TheWhale01 <ard.rasp01@gmail.com>
2024-12-23 20:49:33 +0100

    cursor support

--- src/kernel.c
+++ src/kernel.c
@@ -2,15 +2,23 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-// #if defined(__linux__)
-// #error "You are not using a cross-compiler, you will most certainly run into trouble"
-// #endif
-
 /* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
+
+static inline uint8_t inb (uint16_t __port)
+{
+	uint8_t _v;
+
+	__asm__ volatile ("inb %w1,%0":"=a" (_v):"Nd" (__port));
+	return _v;
+}
+
+static inline void outb (uint16_t __port, uint8_t __value)
+{
+	__asm__ volatile ("outb %b0,%w1": :"a" (__value), "Nd" (__port));
+}
 
 /* Hardware text mode color constants. */
 enum vga_color {
@@ -37,10 +45,12 @@ static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 	return fg | bg << 4;
 }
 
+// Returns value for character + color
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
+
 size_t strlen(const char *str) {
 	size_t len = 0;
 	while (str[len])
@@ -48,7 +58,7 @@ size_t strlen(const char *str) {
 	return len;
 }
 
-// Screen size
+// VGA Screen size
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
@@ -57,12 +67,34 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
+void enable_cursor(uint8_t start, uint8_t end) {
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | start);
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | end);
+}
+
+void disable_cursor(void) {
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
+
+void update_cursor(int x, int y) {
+	uint16_t pos = y * VGA_WIDTH + x;
+
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t)(pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
+
 void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000; // bizarre
+	terminal_buffer = (uint16_t*) 0xB8000; // Buffer for screen
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -90,7 +122,6 @@ void terminal_putchar(char c)
 			++terminal_row;
 			break;
 		default:
-			// Prints on screen ?
 			terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 			if (++terminal_column == VGA_WIDTH) {
 				terminal_column = 0;
@@ -110,16 +141,16 @@ void terminal_write(const char* data, size_t size)
 void terminal_writestring(const char* data)
 {
 	terminal_write(data, strlen(data));
+	update_cursor(terminal_column, terminal_row);
 }
 
 void kernel_main(void)
 {
 	/* Initialize terminal interface */
+	const char *welcome_str = "BonsoirOS\n";
+
+	enable_cursor(0, 15);
 	terminal_initialize();
-
-	// Setting font color
-	terminal_setcolor(vga_entry_color(VGA_COLOR_BLACK, VGA_COLOR_WHITE));
-
-	/* Newline support is left as an exercise. */
-	terminal_writestring("42\nbonsoir");
+	terminal_writestring(welcome_str);
+	// disable_cursor();
 }
