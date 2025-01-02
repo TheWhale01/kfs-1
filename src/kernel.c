549c1db557d502e53e41a0911a4b9dc6e50ba936
Hugo BRETECHE <hubretec@paul-f4Br3s3.clusters.42paris.fr>
2025-01-02 13:40:33 +0100

    finished

--- src/kernel.c
+++ src/kernel.c
@@ -5,7 +5,7 @@ void kernel_main(void)
 	term_data t_data;
 	cursor_data c_data;
 	t_data.cursor = &c_data;
-	const char *welcome_str = "42\n";
+	const char *welcome_str = "42\nsalut !";
 
 	terminal_initialize(&t_data);
 	cursor_enable(&c_data);
