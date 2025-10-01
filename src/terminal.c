bc6dfd4cf93959f373064ef460185c606a1794d1
Rossetti <jules.rossetti.pro@gmail.com>
2025-10-01 19:50:02 +0200

    stack add nbr

--- src/terminal.c
+++ src/terminal.c
@@ -5,137 +5,119 @@
 
 terminal_t terminal = {
 	.screen = 0,
-   .fcolor = VGA_COLOR_WHITE,
-   .bcolor = VGA_COLOR_BLACK,
-   .VGA_MEMORY = (volatile uint16_t* const)0xB8000,
+	.fcolor = VGA_COLOR_WHITE,
+	.bcolor = VGA_COLOR_BLACK,
+	.VGA_MEMORY = (volatile uint16_t* const)0xB8000,
 };
 
 inline void vga_entry(unsigned char c) {
 	terminal.VGA_MEMORY[cursor.VGA_Y[terminal.screen] * VGA_WIDTH + cursor.VGA_X[terminal.screen]]
         = (uint16_t)c | (uint16_t)(terminal.fcolor | terminal.bcolor << 4) << 8;
 }
-void stack() {
-    // uint32_t* esp;
-    // asm volatile ("mov %%esp, %0" : "=r"(esp));
-    // printf("Kernel stack dump\n\n");
-    // for (int i = 0; i < 64; i++) {
-    //     printf("%X    ", (uint32_t)(esp + i));
-	// 	if ((i % 6 == 0))
-	// 		printf("\n\n");
-    // }
-    uint32_t *esp_snapshot;
-    asm volatile ("mov %%esp, %0" : "=r"(esp_snapshot));
+void stack(int nb) {
+	if (nb < 0 || nb > 16884 - 60)
+		nb = 0;
+	uint32_t *esp_snapshot;
+	asm volatile ("mov %%esp, %0" : "=r"(esp_snapshot));
 
-    printf("\n=== Kernel stack dump ===\n");
-    printf("ESP snapshot = %p\n\n", (void*)esp_snapshot);
-	unsigned nb = 2000;
-    for (unsigned i = nb; i < nb + 10; ++i) {
-        for (unsigned j = 0; j < 3; ++j) {
-            uint32_t *addr = esp_snapshot + i*3 + j;
-            uint32_t  val  = *addr;
-            printf("%p: %X       ", (void*)addr, val);
-        }
-        printf("\n");
-    }
+	printf("\n=== Kernel stack dump ===\n");
+	printf("ESP snapshot = %p index : %d to %d\n\n", (void*)esp_snapshot, nb, nb + 60);
+	for (int i = nb; i < nb + 10; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			uint32_t *addr = esp_snapshot + i*3 + j;
+			uint32_t  val  = *addr;
+			printf("%p: %X       ", (void*)addr, val);
+		}
+		printf("\n");
+	}
 }
-
-//stack
-// void reboot();
-// void clear();
-// halt == poweroff
-// Parser espace
-// DEBUG message, ERROR message etc.
-// setcolor [red, blue; green]
-// screen [index]
-//gérer le backspace
 
 void debug(const char *buff) {(void)buff;}
 
 
 void setcolor(const char *buff) {
-    size_t arg1_len = ft_strnlen(buff, ' ');
-    const char *colors[] = {"black", "blue", "green", "cyan", "red", "magenta",
-        "brown", "light_grey", "dark_grey", "light_blue", "light_green", "light_cyan",
-        "light_red", "light_magenta", "light_brown", "white"};
+	size_t arg1_len = ft_strnlen(buff, ' ');
+	const char *colors[] = {"black", "blue", "green", "cyan", "red", "magenta",
+		"brown", "light_grey", "dark_grey", "light_blue", "light_green", "light_cyan",
+		"light_red", "light_magenta", "light_brown", "white"};
 
-    for (size_t i = 0; i < 16; i++) {
-        if (!ft_strncmp(colors[i], buff, ft_strlen(colors[i])) && ft_strlen(colors[i]) == arg1_len) {
-            terminal.fcolor = i;
-            break;
-        }
-    }
-    for (size_t i = 0; i < 16; i++) {
-        if (!ft_strncmp(colors[i], buff + arg1_len + 1, ft_strlen(colors[i])) && ft_strlen(colors[i]) == ft_strlen(buff + arg1_len + 1)) {
-            terminal.bcolor = i;
-            break;
-        }
-    }
+	for (size_t i = 0; i < 16; i++) {
+		if (!ft_strncmp(colors[i], buff, ft_strlen(colors[i])) && ft_strlen(colors[i]) == arg1_len) {
+			terminal.fcolor = i;
+			break;
+		}
+	}
+	for (size_t i = 0; i < 16; i++) {
+		if (!ft_strncmp(colors[i], buff + arg1_len + 1, ft_strlen(colors[i])) && ft_strlen(colors[i]) == ft_strlen(buff + arg1_len + 1)) {
+			terminal.bcolor = i;
+			break;
+		}
+	}
 }
 
 void info() {
-    printf("\nCommande      Syntaxe / Usage                         Description\n");
-    printf("--------------------------------------------------------------------------\n");
-    printf("screen        screen   <1..4>               Selectionne l'ecran 1 a 4\n");
-    printf("clear         clear                         Efface l'ecran\n");
-    printf("setcolor      setcolor <fcolor> <bcolor>    Change les couleurs texte/fond\n");
-    printf("debug         debug    <priority> <msg>     Log de debug\n");
-    printf("priority      priority                      Liste des priorites\n");
-    printf("stack         stack                         Affiche la stack courante\n");
-    printf("reboot        reboot                        Redemarre la machine\n");
-    printf("shutdown      shutdown                      Eteint la machine\n");
+	printf("\nCommande      Syntaxe / Usage                         Description\n");
+	printf("--------------------------------------------------------------------------\n");
+	printf("screen        screen   <1..4>               Selectionne l'ecran 1 a 4\n");
+	printf("clear         clear                         Efface l'ecran\n");
+	printf("setcolor      setcolor <fcolor> <bcolor>    Change les couleurs texte/fond\n");
+	printf("debug         debug    <priority> <msg>     Log de debug\n");
+	printf("priority      priority                      Liste des priorites\n");
+	printf("stack         stack    <index>              Affiche la stack courante\n");
+	printf("reboot        reboot                        Redemarre la machine\n");
+	printf("shutdown      shutdown                      Eteint la machine\n");
 }
 
 void priority()
 {
 	printf("Priority Messages List: \n[EMERGENCY] [ALERT] [CRITICAL] [ERROR] [WARNING] [NOTICE] [INFO] [DEBUG]\n");
 }
+
 void halt() {
 	return;
 }
 
-
-
 void handle_cmd() {
-    char buff[VGA_WIDTH];
-    size_t word_len = ft_strnlen(terminal.CMD_BUFFER[terminal.screen], ' ');
+	char buff[VGA_WIDTH];
+	size_t word_len = ft_strnlen(terminal.CMD_BUFFER[terminal.screen], ' ');
 
-    if (terminal.CMD_BUFFER[terminal.screen][0] == '\0')
-        return ;
-    ft_strlcpy(buff, terminal.CMD_BUFFER[terminal.screen], VGA_WIDTH);
-    ft_bzero(terminal.CMD_BUFFER[terminal.screen], VGA_WIDTH);
-    if (!ft_strncmp(buff, "info", ft_strlen("info")) && ft_strlen("info") == word_len)
+	if (terminal.CMD_BUFFER[terminal.screen][0] == '\0')
+		return ;
+	ft_strlcpy(buff, terminal.CMD_BUFFER[terminal.screen], VGA_WIDTH);
+	ft_bzero(terminal.CMD_BUFFER[terminal.screen], VGA_WIDTH);
+	if (!ft_strncmp(buff, "info", ft_strlen("info")) && ft_strlen("info") == word_len)
 		info();
-    else if (!ft_strncmp(buff, "stack", ft_strlen("stack")) && ft_strlen("stack") == word_len)
-        stack();
-    else if (!ft_strncmp(buff, "reboot", ft_strlen("reboot")) && ft_strlen("reboot") == word_len)
-    	reboot();
-    else if (!ft_strncmp(buff, "shutdown", ft_strlen("shutdown")) && ft_strlen("shutdown") == word_len)
+	else if (!ft_strncmp(buff, "stack", ft_strlen("stack")) && ft_strlen("stack") == word_len)
+		stack(ft_atoi(buff + word_len + 1));
+	else if (!ft_strncmp(buff, "reboot", ft_strlen("reboot")) && ft_strlen("reboot") == word_len)
+		reboot();
+	else if (!ft_strncmp(buff, "shutdown", ft_strlen("shutdown")) && ft_strlen("shutdown") == word_len)
 		halt();
-    else if (!ft_strncmp(buff, "clear", ft_strlen("clear")) && ft_strlen("clear") == word_len)
-        clearscr();
-    else if (!ft_strncmp(buff, "debug", ft_strlen("debug")) && ft_strlen("debug") == word_len)
-        debug(buff + word_len + 1);
-    else if (!ft_strncmp(buff, "priority", ft_strlen("priority")) && ft_strlen("priority") == word_len)
+	else if (!ft_strncmp(buff, "clear", ft_strlen("clear")) && ft_strlen("clear") == word_len)
+		clearscr();
+	else if (!ft_strncmp(buff, "debug", ft_strlen("debug")) && ft_strlen("debug") == word_len)
+		debug(buff + word_len + 1);
+	else if (!ft_strncmp(buff, "priority", ft_strlen("priority")) && ft_strlen("priority") == word_len)
 		priority();
-    else if (!ft_strncmp(buff, "setcolor", ft_strlen("setcolor")) && ft_strlen("setcolor") == word_len)
-        setcolor(buff + word_len + 1);
-    else if (!ft_strncmp(buff, "screen", ft_strlen("screen")) && ft_strlen("screen") == word_len)
-        change_screen(ft_atoi(buff + word_len + 1) - 1);
+	else if (!ft_strncmp(buff, "setcolor", ft_strlen("setcolor")) && ft_strlen("setcolor") == word_len)
+		setcolor(buff + word_len + 1);
+	else if (!ft_strncmp(buff, "screen", ft_strlen("screen")) && ft_strlen("screen") == word_len)
+		change_screen(ft_atoi(buff + word_len + 1) - 1);
 }
 
 bool check_echappement(char c) {
 	switch (c)
 	{
 		case '\n':
-		    line_break();
-		    handle_cmd();
+			line_break();
+			handle_cmd();
 			return (true);
 		case '\a':
 			return (true);
 		case '\b': {
-    			size_t len = ft_strlen(terminal.CMD_BUFFER[terminal.screen]);
-    			terminal.CMD_BUFFER[terminal.screen][len - 1] = '\0';
-    		}
+				size_t len = ft_strlen(terminal.CMD_BUFFER[terminal.screen]);
+				terminal.CMD_BUFFER[terminal.screen][len - 1] = '\0';
+			}
 			line_backspace();
 			return (true);
 		case '\t':
@@ -154,10 +136,10 @@ bool check_echappement(char c) {
 }
 
 void vga_putchar(char c) {
-    if (check_echappement(c)) {
-        return ;
-    }
-   	vga_entry(c);
+	if (check_echappement(c)) {
+		return ;
+	}
+	vga_entry(c);
 	if (++cursor.VGA_X[terminal.screen] >= VGA_WIDTH) {
 		cursor.VGA_X[terminal.screen] = 0;
 		if (cursor.VGA_Y[terminal.screen] == VGA_HEIGHT - 1)
@@ -190,7 +172,7 @@ void scroll(void) {
 }
 
 void change_screen(size_t new_screen) {
-	if (new_screen == terminal.screen)
+	if (new_screen == terminal.screen || new_screen > 3)
 		return ;
 	ft_memcpy(terminal.VGA_SCREEN[terminal.screen],
 		(const char *)terminal.VGA_MEMORY, VGA_WIDTH * VGA_HEIGHT);
